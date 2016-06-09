3e0792a65af9293bae3748eac227a24bc1063592
sachinwak <sachinw68@gmail.com>
2016-06-09 15:43:41 +0530

    Add files via upload
    New files are uploaded

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,11 @@
-// Dropdown Menu Fade    
 jQuery(document).ready(function(){
-    $(".dropdown").hover(
-        function() { $('.dropdown-menu', this).stop().fadeIn("fast");
-        },
-        function() { $('.dropdown-menu', this).stop().fadeOut("fast");
-    });
+	
+	    if (window.matchMedia('(min-width: 768px)').matches) {
+	        $(".dropdown").hover(
+		        function() { $('.dropdown-menu', this).stop().fadeIn("fast");
+		        },
+		        function() { $('.dropdown-menu', this).stop().fadeOut("fast");
+		    });
+	    } 
+    
 });
